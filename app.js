fb5bf8fd707198bc4f4f710786a04105e8cf4918
Jase Cutler <jasecutler@gmail.com>
2017-08-06 22:52:18 -0700

    Working on integrating passport

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const aboutRouter = require('./routes/aboutRoute')
 const loginRouter = require('./routes/loginRoute')
 const session = require('express-session')
 const bodyParser = require('body-parser')
+const passport = require('passport'),
+ LocalStrategy = require('passport-local').Strategy;
+const dbModel = require('./models/db.js')
+const flash = require('express-flash-messages');
+
+//Passport necessary code
 
 //Basic body parser settings
 
@@ -21,17 +27,69 @@ app.set('view engine', 'mustache')
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
-  saveUninitialized: true
+  saveUninitialized: false
 }))
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        dbModel.User.authenticate(username, password, function(err, user) {
+          console.log("Hello")
+            if (err) {
+                return done(err)
+            }
+            if (user) {
+                return done(null, user)
+            } else {
+                return done(null, false, {
+                    message: "There is no user with that username and password."
+                })
+            }
+        })
+    }));
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
 //Allows for a static directory for hosting CSS and other files
 app.use(express.static(path.join(__dirname, 'static')))
 
+
+
 //Use the routes files.
-app.use('/', indexRouter);
-app.use('/about', aboutRouter);
-app.use('/login', loginRouter);
+app.use('/',authenticationMiddleware, indexRouter);
+app.get('/login/', function(req, res) {
+    res.render("login", {
+        messages: res.locals.getMessages()
+    });
+});
+
+app.post('/login/', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login/',
+    failureFlash: true
+}))
+app.use('/about', authenticationMiddleware, aboutRouter);
+//app.use('/login', loginRouter);
 
 app.listen(3000, function(){
   console.log("App running on port 3000")
 })
+
+
+function authenticationMiddleware () {  
+	return (req, res, next) => {
+		console.log(`req.session.passport.user: ${JSON.stringify(req.session.passport)}`);
+
+	    if (req.isAuthenticated()) return next();
+	    res.redirect('/login')
+	}
+}
